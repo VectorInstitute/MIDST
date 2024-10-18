9b3238e60ed62099491846af07e585e057926ef0
John Jewell <taylorjewell@outlook.com>
2024-10-18 12:28:54 -0400

    First pass at black box single table
    Update codabench black box single table bundle with functional scoring for dev and eval phases. Updated blackbox single table starter kit with updated output format for submissions.

--- codabench_bundles/midst_blackbox_single_table/scoring_program/scoring.py
+++ codabench_bundles/midst_blackbox_single_table/scoring_program/scoring.py
@@ -1,26 +1,61 @@
-import json
 import os
+import json
 import numpy as np
-from sklearn.metrics import accuracy_score
 
-reference_dir = os.path.join('/app/input/', 'ref')
-prediction_dir = os.path.join('/app/input/', 'res')
-score_dir = '/app/output/'
+from sklearn.metrics import roc_curve
 
-print('Reading prediction')
-prediction = np.genfromtxt(os.path.join(prediction_dir, 'prediction'))
-truth = np.genfromtxt(os.path.join(reference_dir, 'testing_label'))
-with open(os.path.join(prediction_dir, 'metadata.json')) as f:
-    duration = json.load(f).get('duration', -1)
 
-print('Checking Accuracy')
-accuracy = accuracy_score(truth, prediction)
-print('Scores:')
-scores = {
-    'accuracy': accuracy,
-    'duration': duration
-}
-print(scores)
+def get_tpr_at_fpr(true_membership: np.ndarray, predictions: np.ndarray, max_fpr=0.1) -> float:
+    """Calculates the best True Positive Rate when the False Positive Rate is
+    at most `max_fpr`.
 
-with open(os.path.join(score_dir, 'scores.json'), 'w') as score_file:
-    score_file.write(json.dumps(scores))
+    Args:
+        true_membership (List): A list of values in {0,1} indicating the membership of a
+            challenge point. 0: "non-member", 1: "member".
+        predictions (List): A list of values in the range [0,1] indicating the confidence
+            that a challenge point is a member. The closer the value to 1, the more
+            confident the predictor is about the hypothesis that the challenge point is
+            a member.
+        max_fpr (float, optional): Threshold on the FPR. Defaults to 0.1.
+
+    Returns:
+        float: The TPR @ `max_fpr` FPR.
+    """
+    fpr, tpr, _ = roc_curve(true_membership, predictions)
+
+    return max(tpr[fpr < max_fpr])
+
+
+def score(solutions: np.ndarray, predictions: np.ndarray) -> float:
+    return get_tpr_at_fpr(solutions, predictions) 
+
+
+if __name__ == "__main__":
+    solutions_dir = os.path.join('/app/input/', 'ref')
+    predictions_dir = os.path.join('/app/input/', 'res')
+    output_dir = '/app/output/'
+
+    assert len(os.listdir(predictions_dir)) == len(os.listdir(predictions_dir))
+
+    # We compute the scores globally, across the models. This is somewhat equivalent to having
+    # one attack (threshold) for all the attacks.
+    # Load the predictions.
+    predictions = []
+    solutions  = []
+    for model_id in os.listdir(solutions_dir):
+        solutions.append(np.loadtxt(os.path.join(solutions_dir, model_id, "challenge_label.csv"), skiprows=1))
+        predictions.append(np.loadtxt(os.path.join(predictions_dir, model_id, "prediction.csv")))
+
+    solutions = np.concatenate(solutions)
+    predictions = np.concatenate(predictions)
+
+    # Verify that the predictions are valid.
+    assert len(predictions) == len(solutions)
+    assert np.all(predictions >= 0), "Some predictions are < 0"
+    assert np.all(predictions <= 1), "Some predictions are > 1"
+
+    tpr_at_fpr = score(solutions, predictions)
+    print("TPR at FPR", tpr_at_fpr)
+
+    with open(os.path.join(output_dir, 'scores.json'), 'w') as score_file:
+        score_file.write(json.dumps({"tpr_at_fpr": tpr_at_fpr}))
